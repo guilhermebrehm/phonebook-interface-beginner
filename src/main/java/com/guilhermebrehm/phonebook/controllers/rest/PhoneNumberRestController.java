9a4246e3711345a87471821c4f4832c675ada735
Guilherme Brehm <guilherme.brehm@protonmail.com>
2017-12-23 19:40:05 +0000

    fixed issue with cross origin requests being denied

--- src/main/java/com/guilhermebrehm/phonebook/controllers/rest/PhoneNumberRestController.java
+++ src/main/java/com/guilhermebrehm/phonebook/controllers/rest/PhoneNumberRestController.java
@@ -5,6 +5,7 @@ import com.guilhermebrehm.phonebook.services.phone_number.PhoneNumberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ public class PhoneNumberRestController {
     @Autowired
     PhoneNumberService phoneNumberService;
 
+    @CrossOrigin
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<PhoneNumber>> findAll() {
 
